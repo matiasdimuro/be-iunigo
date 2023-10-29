d68193c8538c2a247ef77de5c7c238a9c0da791a
matiasezequieldimuro <mdimuro@hexagonconsulting.net>
2023-10-29 15:01:01 -0300

    Add: Transitions

--- js/index.js
+++ js/index.js
@@ -22,3 +22,49 @@ window.addEventListener('scroll', () => {
         ? headerNav.classList.add('headerOnScroll')
         : headerNav.classList.remove('headerOnScroll')
 })
+
+const animatedElementsOnLoad = document.querySelectorAll('.AnimatedOnLoad')
+window.addEventListener('DOMContentLoaded', (e) => {
+    animatedElementsOnLoad.forEach(animated => {
+        animated.classList.add('Animation-LeftToRight')
+        animated.classList.remove('BeforeAnimation-LeftToRight')
+    })
+})
+
+const animatedElements = document.querySelectorAll('.Animated')
+const animatedElementsObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const classList = entry.target.classList
+            const classesToHandle = []
+            if (classList.contains("BeforeAnimation-LeftToRight")) {
+                classesToHandle.push(
+                    "BeforeAnimation-LeftToRight", 
+                    "Animation-LeftToRight"
+                )
+            }
+            else if (classList.contains("BeforeAnimation-RightToLeft")) {
+                classesToHandle.push(
+                    "BeforeAnimation-RightToLeft", 
+                    "Animation-RightToLeft"
+                )
+            }
+            else if (classList.contains("BeforeAnimation-TopToBottom")) {
+                classesToHandle.push(
+                    "BeforeAnimation-TopToBottom", 
+                    "Animation-TopToBottom"
+                )
+            }
+            entry.target.classList.add(classesToHandle[1])
+            entry.target.classList.remove(classesToHandle[0])
+            observer.unobserve(entry.target)
+        }
+    })
+}, {
+    root: null,
+    rootMargin: '0px',
+    threshold: .5
+})
+animatedElements.forEach(element => {
+    animatedElementsObserver.observe(element)
+})
